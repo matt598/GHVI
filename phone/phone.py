7b8a82ec3fcf9057e6ef49f45a3b5bf5d2b2b3a1
Greg Cordover <ghc@ixchi.me>
2016-10-23 02:22:53 -0500

    Git Commit Message

--- phone/phone.py
+++ phone/phone.py
@@ -281,7 +281,7 @@ def places(request):
 
     t.say('You may enter a number to notify the location that you are on the way. ')
 
-    t.ask(Choices('1,2,3,4,5'), say='. '.join(spoken_shelters), timeout=120)
+    t.ask(Choices('1,2,3,4,5'), say='. '.join(spoken_shelters), timeout=120, mode='dtmf')
 
     t.on(event='continue', next='/info.json')
 
