20889a6289fd003cbbf09e90894a313c1a53cdd3
Kyle Whitlatch <minerscreedutube@gmail.com>
2016-10-22 20:39:33 -0500

    Finished The User Inputs for The Phone Call

--- phone/phone.go
+++ phone/phone.go
@@ -1,10 +1,15 @@
 package phone
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"github.com/pressly/chi"
 	"net/http"
 )
 
 func GetRouter() http.Handler {
 	r := chi.NewRouter()
+
+	return r
 }
